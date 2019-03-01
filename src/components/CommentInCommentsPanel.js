77713527eb682104bbce3317ce2e734919dee1b1
Jimmy Chu <jimmychu0807@gmail.com>
2019-03-01 23:12:22 +0800

    Show which user commenting at when

--- src/components/CommentInCommentsPanel.js
+++ src/components/CommentInCommentsPanel.js
@@ -1,12 +1,19 @@
 // core/data components
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class CommentInCommentsPanel extends Component {
 
   render() {
     const { comment } = this.props;
+    const { user: commentUser } = comment;
+
     return (<div>
-      { comment.content }
+      <span>{ comment.content }</span>
+      <div>
+        <Link to={ `/profile/${commentUser.id}` }>{ commentUser.name }</Link>
+      </div>
+      <div>{ comment.createdAt }</div>
     </div>)
   }
 }
